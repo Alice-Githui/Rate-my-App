34beb8b876c7fd51ade0d79223cdb23fc32cb7d7
Alice-Githui <gnyokabi.alice@gmail.com>
2021-05-30 23:07:55 +0300

    Add functionality to rate by usability

--- projects/static/JS/script.js
+++ projects/static/JS/script.js
@@ -41,8 +41,10 @@ const designRateForm=document.querySelector('.design-rate-form')
 const designConfirmBox=document.getElementById('design-confirm-box')
 // get the csrf
 const csrf=document.getElementsByName('csrfmiddlewaretoken')
+// get overall rating
+const overallRating=document.getElementById('overall-rating')
 
-
+// DESIGN RATING 
 //optimise the function to hover over the buttons and get that particular value
 const handleButtonSelect =(size) =>{
     const designChildren=designRateForm.children
@@ -192,4 +194,160 @@ if (designFirst){
     }))
 
 }
+
+// USABILITY RATING
+// get the forms
+const usabilityRateForm=document.querySelector('.usability-rate-form')
+const usabilityConfirmBox=document.getElementById('usability-confirm-box')
+//optimise the function to hover over the buttons and get that particular value
+const handleButtonUsabilitySelect =(size) =>{
+    const usabilityChildren=usabilityRateForm.children
+    for (let i =0; i < usabilityChildren.length; i++){
+        if (i <= size){
+            usabilityChildren[i].classList.add('usability-checked')
+        }
+        else{
+            usabilityChildren[i].classList.remove('usability-checked')
+        } 
+    }
+}
+
+// function to change the background colour once someone hovers over a rating number
+const handleUsabilitySelect=(selection) =>{
+    switch(selection){
+        case 'usability-first':{
+            // designFirst.classList.add('design-checked')
+            // designSecond.classList.remove('design-checked')
+            // designThird.classList.remove('design-checked')
+            // designFourth.classList.remove('design-checked')
+            // designFifth.classList.remove('design-checked')
+            // designSixth.classList.remove('design-checked')
+            // designSeventh.classList.remove('design-checked')
+            // designEighth.classList.remove('design-checked')
+            // designNinth.classList.remove('design-checked')
+            // designTenth.classList.remove('design-checked')
+            handleButtonUsabilitySelect(1)
+            return
+        }
+        case 'usability-second':{
+            handleButtonUsabilitySelect(2)
+            return
+        }
+        case 'usability-third':{
+            handleButtonUsabilitySelect(3)
+            return
+        }
+        case 'usability-fourth':{
+            handleButtonUsabilitySelect(4)
+            return
+        }
+        case 'usability-fifth':{
+            handleButtonUsabilitySelect(5)
+            return
+        }
+        case 'usability-sixth':{
+            handleButtonUsabilitySelect(6)
+            return
+        }
+        case 'usability-seventh':{
+            handleButtonUsabilitySelect(7)
+            return
+        }
+        case 'usability-eighth':{
+            handleButtonUsabilitySelect(8)
+            return
+        }
+        case 'usability-ninth':{
+            handleButtonUsabilitySelect(9)
+            return
+        }
+        case 'usability-tenth':{
+            handleButtonUsabilitySelect(10)
+            return
+        }
+    }
+}
+
+// change the values from string to numeric values
+const getUseNumericValue=(stringValue) =>{
+    let numericUseValue;
+    if (stringValue === 'usability-first'){
+        numericUseValue = 1
+    }
+    else if (stringValue === 'usablity-second'){
+        numericUseValue=2
+    }
+    else if (stringValue === 'usability-third'){
+        numericUseValue=3
+    }
+    else if (stringValue === 'usability-fourth'){
+        numericUseValue=4
+    }
+    else if (stringValue==='usability-fifth'){
+        numericUseValue=5
+    }
+    else if (stringValue==='usability-sixth'){
+        numericUseValue=6
+    }
+    else if (stringValue==='usability-seventh'){
+        numericUseValue=7
+    }
+    else if (stringValue==='usability-eighth'){
+        numericUseValue=8
+    }
+    else if (stringValue==='usability-ninth'){
+        numericUseValue=9
+    }
+    else if (stringValue==='usability-tenth'){
+        numericUseValue=10
+    }
+    else{
+        numericUseValue=0 
+    } 
+    return numericUseValue  
+}
+
+if (usabilityFirst){
+    const usabilityArr=[usabilityFirst, usabilitySecond, usabilityThird, usabilityFourth, usabilityFifth, usabilitySixth, usabilitySeventh, usabilityEighth, usabilityNinth, usabilityTenth]
+
+    usabilityArr.forEach(item => item.addEventListener('mouseover', (event)=>{
+        handleUsabilitySelect(event.target.id)
+    }))
+
+    usabilityArr.forEach(item => item.addEventListener('click', (event)=>{
+        const useVal=event.target.id
+
+
+        usabilityRateForm.addEventListener('submit', (event)=>{
+            event.preventDefault()
+            // const id=event.target.id
+            // console.log(id)
+            const valueUsabilityNum=getUseNumericValue(useVal)
+
+            usabilityConfirmBox.innerHTML =`<p>Usability: ${valueUsabilityNum}</p>`
+
+            // $.ajax({
+            //     type:'POST',
+            //     url:'/rateoneproject/',
+            //     data:{
+            //         'csrfmiddlewaretoken':csrf[0].value,
+            //         // 'el_id':id,
+            //         'val':valueNum
+            //     },
+            //     success: function(response){
+            //         console.log(response)
+            //         designConfirmBox.innerHTML=`<p>${response.design-rate}</p>`
+            //     },
+            //     error: function(error){
+            //         console.log(error)
+            //         designConfirmBox.innerHTML='<p>Oooops .... something went wrong </p>'
+            //     }
+            // })
+        })
+    }))
+
+}
+
+
+
 
